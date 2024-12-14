b60058e1626f56309d7cfe7518d3eafff79cba9d
ichimei0125 <ichimei0125@gmail.com>
2024-12-15 00:06:18 +0900

    bugfix

--- internal/config/envVar.go
+++ internal/config/envVar.go
@@ -3,7 +3,7 @@ package config
 import "os"
 
 func GetEnvVar(envName, _default string) string {
-	config, exist := os.LookupEnv("CONFIG_PATH")
+	config, exist := os.LookupEnv(envName)
 	if !exist {
 		return _default
 	}
