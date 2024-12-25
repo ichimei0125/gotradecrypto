d310c0204d67c50c4274726cc26794f414518aa6
ichimei0125 <ichimei0125@gmail.com>
2024-12-25 11:14:39 +0900

    rename

--- scripts/mac/launchd/load.sh
+++ scripts/mac/launchd/load.sh
@@ -5,6 +5,7 @@ go build -o gotradecypto cmd/main.go
 sudo launchctl list | grep gotradecypto
 sudo launchctl bootout system/localhost.gotradecypto.main
 sudo launchctl list | grep gotradecypto
+sudo chown root:wheel localhost.gotradecypto.main.plist 
 sudo cp scripts/mac/launchd/localhost.gotradecypto.main.plist /Library/LaunchDaemons/
 sudo launchctl bootstrap system /Library/LaunchDaemons/localhost.gotradecypto.main.plist
 sudo launchctl list | grep gotradecypto
